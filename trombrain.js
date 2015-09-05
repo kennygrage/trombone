51ae88294aafdb0a1d1c0b1b172911baff7a7d61
Kenny Grage <kennygrage@gmail.com>
2015-09-05 13:53:02 -0700

    add comments

--- trombrain.js
+++ trombrain.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-	var maxX = 6; //number of rows
-	var maxY = 6; //number of columns
-	var tromArray = [];
+
 
 
 	for (var y = maxY; y >= 0; y--) {
@@ -28,18 +26,19 @@ $(document).ready(function(){
 			}
 
 
-			tromArray[x][y] = xToString + yToString;
-			$("#tromarea").append('<div id="' + tromArray[x][y] + '"></div>');
-			$("#" + tromArray[x][y]).addClass("tromclass");
-			$("#" + tromArray[x][y]).append(xToString + yToString);
-			$("#" + tromArray[x][y]).css("background-color",uncolor);
+			tromArray[x][y] = xToString + yToString;														//tromArray[x][y] = div name as a string
+			$("#tromarea").append('<div id="' + tromArray[x][y] + '"></div>');	//make a div with an id of tromArray[x][y]
+			$("#" + tromArray[x][y]).addClass("tromClass");											//add the class "tromClass" to the div
+			$("#" + tromArray[x][y]).append(xToString + yToString);							//write all of the div names in the divs
+			$("#" + tromArray[x][y]).css("background-color",uncolor);						//start div with inital color
 			if (x === 0) {
-				$("#" + tromArray[x][y]).css("clear", "both");
+				$("#" + tromArray[x][y]).css("clear", "both");										// start a new row
 			}
 		}
 
 	}
-	$(".tromclass").css("margin", "5px").css("padding", "5px").css("width", "30px").css("height", "30px").css("float", "left").css("font-size", "x-small");
+	//add all the properties of the div
+	$(".tromClass").css("margin", "5px").css("padding", "5px").css("width", "30px").css("height", "30px").css("float", "left").css("font-size", "x-small");
 });
 
 function sleep(ms) {
@@ -49,36 +48,46 @@ function sleep(ms) {
 }
 
 function initialColoring() {
-	for (var colCharNum = 0; colCharNum < messageArray.length; colCharNum++) {
-		row_which_is_really_column_and_therefore_is_x++;
-		var rowArray = messageArray[colCharNum].split(",");
-		rowArray.shift();
-		for (var eachElement = 0; eachElement < rowArray.length; eachElement++) {
-			if (rowArray[eachElement].length > 1) {
-				//0,0|0,0,0,0,0,0,0
-				rowArray[eachElement] = rowArray[eachElement].substring(2,3);
+	$(document).ready(function(){
+		for (var colCharNum = 0; colCharNum < messageArray.length; colCharNum++) {		//go thru for loop for each number of columns that the letter has
+			row_which_is_really_column_and_therefore_is_x++;
+			var rowArray = messageArray[colCharNum].split(",");													//split each element of the messageArray by ","
+			rowArray.shift();																														//push the top of the letter down one row
+			for (var eachElement = 0; eachElement < rowArray.length; eachElement++) {
+				if (rowArray[eachElement].length > 1) {
+					//0,0|0,0,0,0,0,0,0
+					rowArray[eachElement] = rowArray[eachElement].substring(2,3);						//rowArray[eachElement] is now the third element (the number after the vertical pipe "|")
+				}
+			}
+			for (var eachElement = 0; eachElement < rowArray.length; eachElement++) {
+				rowArray[eachElement] = rowArray[eachElement].split(",");
+
+				if (rowArray[eachElement] == 1) {
+					var idNum = 9000;
+					var color_x = row_which_is_really_column_and_therefore_is_x + maxX - 6;
+					var color_y = eachElement;
+					idNum = idNum + color_x;
+					idNum = idNum.toString();
+					idNum = idNum.concat("00", color_y);
+					idNumsToChange.push( idNum );
+					//document.write(idNum + "<br>");
+					document.write(idNumsToChange.length + "<br>");
+
+				}
 			}
 		}
+	});
+}
 
-		for (var eachElement = 0; eachElement < rowArray.length; eachElement++) {
-			rowArray[eachElement] = rowArray[eachElement].split(",");
-
-			if (rowArray[eachElement] == 1) {
-				var color_x = row_which_is_really_column_and_therefore_is_x;
-				var color_y = eachElement;
-				var idNum = "00";
-				idNum = idNum.concat(color_x, "00", color_y);
-				idNumsToChange.push( idNum );
-			}
-		}
-	}
+function moveLetter() {
+	changeTheDivs();
+	colorTheDivs();
 }
 
 function colorTheDivs() {
 	$(document).ready(function(){
 		while (idNumsToChange.length > 0) {
-			var prefix = "900";
-			var divToColor = prefix.concat(idNumsToChange[0]);
+			var divToColor = idNumsToChange[0];
 			$("#" + divToColor).css("background-color", color);
 			idNumsToChangeBack.push(idNumsToChange[0]);
 			idNumsToChange.shift();
@@ -87,29 +96,34 @@ function colorTheDivs() {
 }
 
 function changeTheDivs() {
+	document.write(idNumsToChangeBack.length);
 	for (var i = 0, len = idNumsToChange.length; i < len; i++) {
+		document.write("Old number - " + idNumsToChange[i]);
 		idNumsToChange[i] = Number(idNumsToChange[i]);
 		idNumsToChange[i] -= 1000;  //move to the left one space
 		idNumsToChange[i].toString();
-
+		document.write(" - New number - " + idNumsToChange[i] + "<br>");
 	}
 }
 
-
+	var maxX = 20; 			//number of rows
+	var maxY = 6;				//number of columns
+	var tromArray = [];	//array that will hold the names of all of the divs
 	var hFont = "0,0|0,0,0,0,0,0,0!0,0|0,0,0,0,0,0,0!0,0|1,1,1,1,1,1,0!0,0|0,0,0,1,0,0,0!0,0|0,0,0,1,0,0,0!0,0|1,1,1,0,0,0,0!0,0|0,0,0,0,0,0,0";
 	var color = "#03d3d3";
 	var uncolor = "#d3d3d3";
-	var messageArray = hFont.split("!");
+	var messageArray = hFont.split("!");										//hold the data for the letter that is to be moved accross the screen - split by each "!"
 	var row_which_is_really_column_and_therefore_is_x=0;
 	var idNumsToChange = [];
 	var idNumsToChangeBack = [];
 
 	initialColoring();
+	document.write("After initialColoring: " + idNumsToChange.length + "<br>");
+	moveLetter();
+	//moveLetter();
 
 
-
-	changeTheDivs();
-	colorTheDivs();
+	//setInterval(function () {changeTheDivs(); colorTheDivs();}, 1000);
 
 	// sleep (1000);
 	//
